201b626b1d92a0d896705bb78744000dee1080b8
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-08-05 18:30:31 -0300

    feat: add solution of the problem 2023-08-18/D

--- 2023-08-18/D/D.cpp
+++ 2023-08-18/D/D.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <cmath>
+#include <math.h>
+
+using namespace std;
 
 int main () {
-    std::cout << "Hello, World!" << std::endl;
+    auto x = unsigned{0};
+    cin >> x;
+    auto current_2_factor = unsigned{0};
+    current_2_factor = static_cast<unsigned>(pow(2, floor(log2(x))));
+    auto number_of_bacterias = unsigned{0};
+    while (x != 0) {
+        if (x >= current_2_factor) {
+            ++number_of_bacterias;
+            x -= current_2_factor;
+        }
+        current_2_factor /= 2;
+    }
+    cout << number_of_bacterias;
     return 0;
 }
