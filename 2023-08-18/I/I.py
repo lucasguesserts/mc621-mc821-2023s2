73c195f68990f49ba48195acf87bb6fcc09f23d3
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-08-14 22:33:45 -0300

    feat: add another solution of the problem 2023-08-18/I (not accepted)

--- 2023-08-18/I/I.py
+++ 2023-08-18/I/I.py
@@ -1,19 +1,27 @@
+import math
+
+BIG_NUMBER = 10**9 + 7
+
+
 def is_good(n: str, good_numbers: str) -> bool:
     for i in range(len(n)):
-        if (n[i] not in good_numbers):
+        if n[i] not in good_numbers:
             return False
     return True
 
-def is_excellent(n: str, good_numbers: str) -> bool:
-    s = str(sum(map(int, n)))
-    return is_good(n, good_numbers) and is_good(s, good_numbers)
 
 if __name__ == "__main__":
     a, b, n = input().split()
     n = int(n)
     good_numbers = a + b
     count = 0
-    for i in range(10**(n-1), 10**(n)):
-        if (is_excellent(str(i), good_numbers)):
-            count += 1
+    for i in range(n + 1):
+        number_of_as = i
+        number_of_bs = n - i
+        sum_of_digits = number_of_as * int(a) + number_of_bs * int(b)
+        if is_good(str(sum_of_digits), good_numbers):
+            count += math.factorial(n) // (
+                math.factorial(number_of_as) * math.factorial(number_of_bs)
+            )
+            count %= BIG_NUMBER
     print(count, end="")
--- 2023-08-18/I/README.md
+++ 2023-08-18/I/README.md
@@ -1,3 +1,10 @@
 # [Beautiful Numbers](https://codeforces.com/problemset/problem/300/C)
 
-Solve it by brute force: generate all numbers with `n` digits and, for each one of them, check if it is excellent.
+
+Notice that we are interested the numbers with length equals `n` (requirement of the problem) with only the digits `a, b` (so they are good).
+
+Suppose one has `n_a` digits `a` and `n_b` digits `b`. One can verify if any number with those digits is excellent by checking if `n_a * a + n_b * b` is good. If that is the case, well, using combinatorics, one knows how many numbers are excellent with those digits: `n! / (n_a! * n_b!) = C(n_a + n_b, n_a)`.
+
+Now, to solve the problem, one has to iterate over all possible values of `n_a` and `n_b` such that `n_a + n_b = n` and sum the quantity of number which are excellent.
+
+There is a catch: one has to efficiently compute the number of combinations modulo `10^9 + 7`.
