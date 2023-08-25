34afe6c03f4aec65f7b6bae52aa3961c9bee5942
Samuel Plaça <samuplaza@gmail.com>
2023-08-24 22:22:31 -0300

    feat: add solution to problem 2023-08-25/E

--- 2023-08-25/E/E.cpp
+++ 2023-08-25/E/E.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
 
+#define MAXLEN  5
+
 using namespace std;
 
+unsigned positive_combinations(char pattern[MAXLEN+1], int start) {
+    if (!pattern[start]) return 1;
+    char c = pattern[start];
+    unsigned this_position = 1;
+    if (c == '?') this_position = (start) ? 10 : 9;
+    else if (c == '0' && !start) return 0;
+    return this_position*positive_combinations(pattern, start+1);
+}
+
 int main () {
-    cout << "Hello, World!" << endl;
+    int ncases;
+    char input[MAXLEN+1];
+    cin >> ncases;
+    while (ncases--) {
+        cin >> input;
+        cout << positive_combinations(input, 0) << endl;
+    }
     return 0;
 }
--- 2023-08-25/E/README.md
+++ 2023-08-25/E/README.md
@@ -1,2 +1,8 @@
-# [PROBLEM_NAME](PROBLEM_LINK)
+# [Matching](https://codeforces.com/problemset/problem/1821/A)
 
+This problem requires counting the amount of characters `?` in the input.
+Each `?` multiplies the amount of combinations by 10, except for a `?` at the
+very start of the string, since leading zeroes are not considered valid.
+
+It is also important to discard the pattern if it starts with `0` -- it is
+either zero (and we're counting POSITIVE matches) or an invalid pattern.
