fbe0562ce2cdb41539784c6c25378e07b5e1d952
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-08-28 15:16:50 -0300

    feat: add solution of of problem 2023-09-01/A

--- 2023-09-01/A/A.cpp
+++ 2023-09-01/A/A.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 int main () {
-    cout << "Hello, World!" << endl;
+    auto B = int(1);
+    auto S = int(1);
+    auto count = int(0);
+    while (true) {
+        ++count;
+        cin >> B >> S;
+        if ((B == 0) && (S == 0)) {
+            break;
+        }
+        cout << "Case " << count << ": ";
+        if (B == 1) {
+            cout << ":-\\" << endl;
+            continue;
+        }
+        auto probability_before = min(double(S) / B, 1.0);
+        auto probability_after = min(double(S - 1) / (B - 1), 1.0);
+        if (probability_after > probability_before) {
+            cout << ":-)" << endl;
+        } else if (probability_after < probability_before) {
+            cout << ":-(" << endl;
+        } else {
+            cout << ":-|" << endl;
+        }
+    }
     return 0;
 }
--- 2023-09-01/A/README.md
+++ 2023-09-01/A/README.md
@@ -1,2 +1,19 @@
-# [PROBLEM_NAME](PROBLEM_LINK)
+# [12114 - Bachelor Arithmetic](https://onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=3266)
+
+```plain
+if (B == 1)
+    print :-\
+
+before = min(1, S/B)
+after = min(1, (S-1)/(B-1))
+
+if (after < before)
+    print :-(
+if (after > before)
+    print :-)
+if after == before
+    print :-|
+```
+
+Yes, it is that simple!
 
