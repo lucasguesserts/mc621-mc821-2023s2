318401662c8d5a65e624714950661e132ac184ae
Samuel Plaça <samuplaza@gmail.com>
2023-08-29 02:43:23 -0300

    feat: add solution to 2023-09-01/G

--- 2023-09-01/G/G.cpp
+++ 2023-09-01/G/G.cpp
@@ -1,8 +1,109 @@
+#include <cstdio>
 #include <iostream>
+#include <cstdint>
+ 
+#define Vertex int
+#define value_t uint64_t
 
-using namespace std;
+#define maxV 128
 
-int main () {
-    cout << "Hello, World!" << endl;
+// Graph data structure
+value_t adj[maxV][maxV];
+int numV;
+
+void GRAPHinsertE(Vertex v, Vertex w, value_t prob) {
+    adj[v][w] = adj[w][v] = prob;
+}
+
+// probabilities and search tree parents of each vertex
+double prob[maxV];
+Vertex parnt[maxV];
+
+// priority queue data structure
+Vertex PQ[maxV];
+int PQsz;
+ 
+void PQinit() {
+    PQsz = 0;
+}
+ 
+int PQempty() {
+    return (PQsz == 0);
+}
+ 
+void PQinsert(Vertex v) {
+    PQ[PQsz++] = v;
+}
+ 
+Vertex PQdelmax() {
+    Vertex v;
+    int i, max_ind;
+    double max_prob = 0.0;
+    for (i = 0; i < PQsz; i++)
+        if (prob[PQ[i]] > max_prob) {
+            max_prob = prob[PQ[i]];
+            max_ind = i;
+        }
+    v = PQ[max_ind];
+    PQ[max_ind] = PQ[--PQsz];
+    return v;
+}
+ 
+double max_prob() {
+    Vertex v, w;
+    double prob_aux;
+    for (v = 0; v < numV; v++) {
+        prob[v] = 0.0;
+        parnt[v] =  -1;
+    }
+ 
+    PQinit();
+ 
+    parnt[0] = 0;
+    prob[0] = 100.0;
+    PQinsert(0);
+ 
+    while (!PQempty()) {
+        v = PQdelmax();
+        for (w = 0; w < numV; w++)
+            if (adj[v][w] > 0) {
+                prob_aux = prob[v]*(adj[v][w])/100.0;
+                if (parnt[w] == -1) {
+                    parnt[w] = v;
+                    prob[w] = prob_aux;
+                    PQinsert(w);
+                }
+                else if (prob_aux > prob[w]) {
+                    parnt[w] = v;
+                    prob[w] = prob_aux;
+                }
+            }
+    }
+ 
+ 
+    return prob[numV-1];
+}
+ 
+void GRAPHinit() {
+    int i, j;
+    for (i = 0; i < numV; i++)
+        for (j = 0; j < numV; j++)
+            adj[i][j] = 0;
+}
+ 
+int main() {
+    int m;
+    while ((scanf("%d %d", &numV, &m) != -1) && numV) {
+        GRAPHinit();
+        while (m--) {
+            int i, j;
+            unsigned prob;
+            std::cin >> i >> j >> prob;
+            GRAPHinsertE(i-1, j-1, prob);
+        }
+        printf("%.6f percent\n", max_prob());
+    }
+ 
     return 0;
 }
+
--- 2023-09-01/G/README.md
+++ 2023-09-01/G/README.md
@@ -1,2 +1,12 @@
-# [PROBLEM_NAME](PROBLEM_LINK)
+# [106 miles to Chicago](https://www.spoj.com/problems/CHICAGO/)
+
+We want to find a maximum cost path in the graph.
+Can be done modifying Dijktra or Floyd-Warshall algorithms.
+
+Needed modifications:
+
+* Instead of compounding edge values by addition, compound by multiplying;
+* Instead of minimizing the total value (cost) of the path, maximize the
+total value (probability).
+
 
