314031c4eb15de9760e5befc8c3e97fbe45847cc
Samuel <samuplaza@gmail.com>
2023-09-06 10:57:30 -0300

    feat: add solution to 2023-09-15/A

--- 2023-09-15/A/A.cpp
+++ 2023-09-15/A/A.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
+#include <bitset>
+#include <vector>
+#include <cstdint>
+
+#define MAX_SIEVE_SIZE      40000   // > SQRT(MAXN) 
 
 using namespace std;
 
-int main () {
-    cout << "Hello, World!" << endl;
+vector<uint32_t>        _primes;
+uint32_t                _sieve_size;
+bitset<MAX_SIEVE_SIZE>  _is_prime;
+
+void sieve(uint32_t sieve_limit) {
+    _sieve_size = sieve_limit + 1;
+    _is_prime.set();
+    _is_prime[0] = _is_prime[1] = 0;
+    for (uint32_t i = 0; i < _sieve_size; i++) {
+        if (!_is_prime[i]) continue;
+        _primes.push_back(i);
+        for (uint32_t j = i*i; j < _sieve_size; j += i) _is_prime[j] = 0;
+    }
+}
+
+bool is_prime(uint64_t n) {
+    if (n < _sieve_size) return _is_prime[n];
+    for (auto p : _primes)
+        if (!(n % p)) return false;
+    return true;
+}
+
+int main() {
+    sieve(MAX_SIEVE_SIZE);
+    uint32_t n;
+    while ((cin >> n) && n) {
+        uint32_t candidate;
+        for (candidate = 2*n + 1; !is_prime(candidate); candidate++);
+        cout << candidate;
+        if (!is_prime(n))
+            cout << " (" << n << " is not prime)";
+        cout << endl;
+    }
+
+
     return 0;
 }
--- 2023-09-15/A/README.md
+++ 2023-09-15/A/README.md
@@ -1,2 +1,7 @@
-# [PROBLEM_NAME](PROBLEM_LINK)
+# [Enlarging Hash Tables](https://open.kattis.com/problems/enlarginghashtables)
 
+One only needs to test $2n+1, 2n+2, \ldots$ for primality. Here we implement a solution using a sieve
+up to $40000$ since $n$ can be up to $2^{30}-1$ and thus we should aim for a sieve up to a good margin
+over $\sqrt{2n}$.
+
+It should be noted that this is an easy problem that does not actually require the use of a sieve to pass.
