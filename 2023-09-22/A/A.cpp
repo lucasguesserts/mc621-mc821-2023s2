31bb62610443d3aec0a289ee70adc32b1be5a2b5
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-09-18 10:22:43 -0300

    feat: add solution to 2023-09-22/A

--- 2023-09-22/A/A.cpp
+++ 2023-09-22/A/A.cpp
@@ -2,7 +2,25 @@
 
 using namespace std;
 
-int main () {
-    cout << "Hello, World!" << endl;
-    return 0;
+using N = long long int;
+
+N find_smallest_divisor(N n) {
+  for (N i = 1; i <= n; ++i) {
+    if (n % i != 0) {
+      return i - 1;
+    }
+  }
+  return n;
+}
+
+int main() {
+  N t;
+  cin >> t;
+  while (t--) {
+    N n;
+    cin >> n;
+    N x = find_smallest_divisor(n);
+    cout << x << endl;
+  }
+  return 0;
 }
