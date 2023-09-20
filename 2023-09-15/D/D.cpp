d8a5f5feb34c38c5d543f7fa6d01fcff784edbd3
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-09-20 20:10:40 -0300

    refactor: minor changes

--- 2023-09-15/D/D.cpp
+++ 2023-09-15/D/D.cpp
@@ -1,29 +1,29 @@
 #include <algorithm>
 #include <cmath>
-#include <iostream>
 #include <cstdint>
-
-#define MAXN    200000
+#include <iostream>
 
 using namespace std;
 
-int64_t gcd(int64_t a, int64_t b) { return (b == 0) ? a : gcd(b, a%b); }
+using I = int64_t;
+
+I gcd(I a, I b) { return (b == 0) ? a : gcd(b, a % b); }
 
 int main() {
-    int len1, len2;
-    int64_t a0, a, diff, GCD_a, b;
-    cin >> len1 >> len2;
-    cin >> a0;
-    GCD_a = 0;
-    for (int i = 1; i < len1; i++) {
-        cin >> a;
-        diff = (a > a0) ? a-a0 : a0-a;
-        GCD_a = gcd(diff, GCD_a);
-    }
-    for (int i = 0; i < len2; i++) {
-        cin >> b;
-        cout << gcd(a0 + b, GCD_a) << " ";
-    }
-    cout << endl;
-    return 0;
+  I len1, len2;
+  I a0, a, diff, GCD_a, b;
+  cin >> len1 >> len2;
+  cin >> a0;
+  GCD_a = 0;
+  for (I i = 1; i < len1; i++) {
+    cin >> a;
+    diff = (a > a0) ? a - a0 : a0 - a;
+    GCD_a = gcd(diff, GCD_a);
+  }
+  for (I i = 0; i < len2; i++) {
+    cin >> b;
+    cout << gcd(a0 + b, GCD_a) << " ";
+  }
+  cout << endl;
+  return 0;
 }
