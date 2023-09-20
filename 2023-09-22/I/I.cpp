82e73bc00bc49a9ef36c1955c2c18fc3416c647d
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-09-19 21:49:00 -0300

    feat: add solution to 2023-09-22/I

--- 2023-09-22/I/I.cpp
+++ 2023-09-22/I/I.cpp
@@ -2,7 +2,34 @@
 
 using namespace std;
 
+using I = long long int;
+
+constexpr I M = 998244353;
+
+I exp(I y) {
+    if (y == 0) {
+        return I(1);
+    } else if (y % 2 == 0) {
+        I z = exp(y / 2);
+        z *= z;
+        z %= M;
+        return z;
+    } else {
+        I z1 = exp(y - 1);
+        I z = 2;
+        z *= z1;
+        z %= M;
+        return z;
+    }
+}
+
 int main () {
-    cout << "Hello, World!" << endl;
+    I w, h;
+    cin >> w >> h;
+    I W = exp(w);
+    I H = exp(h);
+    I x = W * H;
+    x %= M;
+    cout << x;
     return 0;
 }
