0e7547d0ddc1e90bf5796a3ec62332acd6b3d8fc
Samuel Plaça <samuplaza@gmail.com>
2023-10-05 20:34:53 -0300

    feat: add solution to 2023-10-06/C

--- 2023-10-06/C/C.cpp
+++ 2023-10-06/C/C.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+unsigned shortest_starting(string s, int len) {
+    int first, last;
+    for (first = 0, last = len-1; s[first++] != s[last--] && len; len -= 2);
+    return len;
+}
+
 int main () {
-    cout << "Hello, World!" << endl;
+    int ncases;
+    cin >> ncases;
+    while (ncases--) {
+        int len;
+        string in;
+        cin >> len >> in;
+        cout << shortest_starting(in, len) << endl;
+    }
     return 0;
 }
