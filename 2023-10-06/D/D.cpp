b60e5213ffb7685455ade1b670dfcc92442b0018
Samuel Plaça <samuplaza@gmail.com>
2023-10-06 00:44:24 -0300

    feat: add solution to 2023-10-06/D

--- 2023-10-06/D/D.cpp
+++ 2023-10-06/D/D.cpp
@@ -1,8 +1,50 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main () {
-    cout << "Hello, World!" << endl;
+const int MAX_N = 2000000;
+
+int b[MAX_N];
+
+void kmpPreprocess(string P) {
+    int i = 0, j = -1;
+    b[0] = -1;
+    while (i < P.size()) {
+        while ((j >= 0) && (P[i] != P[j]))
+            j = b[j]; // different, reset j
+        ++i;
+        ++j;
+        b[i] = j;
+    }
+}
+
+unsigned kmpSearch(string P, string T) {
+    int i = 0, j = 0,
+        n = T.size(),
+        m = P.size();
+    unsigned matches = 0;
+    while (i < n) {
+        while ((j >= 0) && (T[i] != P[j]))
+            j = b[j]; // if different, reset j
+        ++i;
+        ++j;
+        if (j == m) {
+            matches++; 
+            j = b[j];
+        }
+    }
+    return matches;
+}
+
+unsigned maximum_power(string s) {
+    kmpPreprocess(s);
+    return kmpSearch(s, s+s)-1;
+}
+
+int main() {
+    string input;
+    while ((cin >> input) && input[0] != '.')
+        cout << maximum_power(input) << endl;
     return 0;
 }
