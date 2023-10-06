0fef83d0e51a0005394f9fbb9d3f3818e53626c1
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-10-05 21:58:03 -0300

    feat: add solution to 2023-10-06/E (not accepted)

--- 2023-10-06/E/E.cpp
+++ 2023-10-06/E/E.cpp
@@ -1,8 +1,58 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main () {
-    cout << "Hello, World!" << endl;
+using I = long long int;
+using VI = vector<I>;
+
+I find(string & s, VI & v, char c, I b) {
+    for (I i = s.size() - 1; i > b; --i) {
+        if (s[i] == c)
+            if (v[i] < v[b])
+                return i;
+    }
+    return I(-1);
+}
+
+void opt(string & s, VI & v) {
+    for (I i = 0; i < s.size(); ++i) {
+        char c = s[i];
+        I p = find(s, v, c, i);
+        if (p != -1) {
+            swap(v[i], v[p]);
+        }
+    }
+    return;
+}
+
+I bubble_sort(VI & arr) {
+    // https://www.geeksforgeeks.org/bubble-sort-in-cpp/
+    I i, j;
+    I n = arr.size();
+    I count = 0;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                ++count;
+            }
+        }
+    }
+    return count;
+}
+
+int main() {
+    I n, count;
+    string s;
+    cin >> n;
+    cin >> s;
+    VI v(n);
+    for (I i = 0; i < n; ++i) {
+        v[i] = n - i - 1;
+    }
+    opt(s, v);
+    cout << bubble_sort(v);
     return 0;
 }
--- 2023-10-06/E/README.md
+++ 2023-10-06/E/README.md
@@ -1,2 +1,9 @@
-# [PROBLEM_NAME](PROBLEM_LINK)
+# [Codeforces 1430E - String Reversal](https://codeforces.com/problemset/problem/1430/E)
 
+Consider the string `s = abcafag` (as an example).
+
+1. map where each character should be in the reverse string:
+   1. `[6, 5, 4, 3, 2, 1, 0]`;
+2. for each char, find the last position where it occurs and swap them;
+   1. character `a`: `[6, 5, 4, 3, 2, 1, 0]` $\rightarrow$ `[1, 5, 4, 3, 2, 6, 0]`
+3. compute how many swap operations a sort needs;
