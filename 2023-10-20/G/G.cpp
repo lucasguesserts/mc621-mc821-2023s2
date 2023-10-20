8260e448c719058a5d5f4b1f712ad0a3855f4e4a
Samuel Plaça <samuplaza@gmail.com>
2023-10-20 01:31:00 -0300

    feat: add solution to 2023-10-20/G

--- 2023-10-20/G/G.cpp
+++ 2023-10-20/G/G.cpp
@@ -1,8 +1,25 @@
 #include <iostream>
+#include <cstdio>
+#include <string>
 
 using namespace std;
 
+bool is_square(string s) {
+    int n = s.length();
+    if (n % 2) return false;
+    int i = 0, j = n/2;
+    while (i < n/2 && j < n)
+        if (s[i++] != s[j++]) return false;
+    return true;
+}
+
 int main () {
-    cout << "Hello, World!" << endl;
+    int n;
+    scanf("%d\n", &n);
+    while (n--) {
+        string s;
+        cin >> s;
+        cout << (is_square(s) ? "YES" : "NO") << endl;
+    }
     return 0;
 }
