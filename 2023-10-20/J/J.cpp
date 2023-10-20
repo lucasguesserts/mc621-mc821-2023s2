aa567f05d6a6286884ffa531a8d9ae1f05e87e12
Samuel Plaça <samuplaza@gmail.com>
2023-10-19 23:25:51 -0300

    feat: add solution to 2023-10-20/J

--- 2023-10-20/J/J.cpp
+++ 2023-10-20/J/J.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <string>
+#include <unordered_set>
 
 using namespace std;
 
+bool is_pangram(string s) {
+    string alphabet = "abcdefghijklmnopqrstuvwxyz";
+    unordered_set<char> found;
+    for (auto c: s) found.insert(tolower(c));
+    for (auto c: alphabet)
+        if (!found.count(c))
+            return false;
+    return true;
+}
 int main () {
-    cout << "Hello, World!" << endl;
+    int n;
+    string in;
+    cin >> n >> in;
+    cout << (is_pangram(in) ? "YES" : "NO") << endl;
     return 0;
 }
