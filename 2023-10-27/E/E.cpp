3d2f6618420a4998cb2d91a328e8b6274d8c61af
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-10-27 07:39:09 -0300

    refactor: rename 2023-10-27/F to 2023-10-27/E

--- 2023-10-27/E/E.cpp
+++ 2023-10-27/E/E.cpp
@@ -2,9 +2,32 @@
 
 using namespace std;
 
-using I = int64_t;
+string get_cycle(string s, int i) {
+    s = s.substr(i) + s.substr(0, i);
+    return s;
+}
+
+int find_min_cycle(const string s) {
+    auto minimum = s;
+    int i_min = 0;
+    for (int i = 0; i < s.size(); i++) {
+        auto cycle = get_cycle(s, i);
+        if (cycle < minimum) {
+            minimum = std::move(cycle);
+            i_min = i;
+        }
+    }
+    return i_min;
+}
 
 int main () {
-    cout << "Hello, World!" << endl;
+    int T;
+    string s;
+    cin >> T;
+    while(T--) {
+        cin >> s;
+        auto i = find_min_cycle(s);
+        cout << i + 1 << endl;
+    }
     return 0;
 }
