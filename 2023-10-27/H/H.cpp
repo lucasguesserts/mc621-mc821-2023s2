03b50aa6d36e0c2242401aa25bd6344cb1a30244
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-10-27 15:20:47 -0300

    feat: add solution to 2023-10-27/H

--- 2023-10-27/H/H.cpp
+++ 2023-10-27/H/H.cpp
@@ -1,10 +1,37 @@
 #include <bits/stdc++.h>
+#include <limits>
 
 using namespace std;
 
-using I = int64_t;
+const string all_letters = "abcdefghijklmnopqrstuvwxyz";
+const int inf = numeric_limits<int>::max();
+
+using M = map<char, int>;
+
+M init() {
+    M m;
+    for (const auto & c : all_letters) {
+        m[c] = inf;
+    }
+    return m;
+}
 
 int main () {
-    cout << "Hello, World!" << endl;
+    int T;
+    string s;
+    cin >> T;
+    while (T--) {
+        cin >> s;
+        auto m = init();
+        int minimum_index = inf;
+        for (int i = s.size() - 1; i >=0; --i) {
+            auto it = m.find(s[i]);
+            if (it->second == inf) {
+                it->second = i;
+                minimum_index = i;
+            }
+        }
+        cout << s.substr(minimum_index, s.size() - minimum_index) << endl;
+    }
     return 0;
 }
--- 2023-10-27/H/README.md
+++ 2023-10-27/H/README.md
@@ -1,2 +1,5 @@
-# [PROBLEM_NAME](PROBLEM_LINK)
+# [Codeforces 1654 B - Prefix Removals](https://codeforces.com/problemset/problem/1654/B)
 
+Let `s` be the input string of size `n` and `c_0` the first character. Notice that, if `c_0` repeats, then in the worst case, the longest prefix is `c_0` and it can be removed. In other words, one can remove the first letter of `s` if it repeats somewhere else in `s`.
+
+To solve the problem, process the string from right (end) to left (beginning) and save the index of the last ocurrence of each character. Notice that such character cannot be removed. Then, take `i` as the smallest of those indices. The answer is the string `s[i..n]` (from the first character that cannot be removed to the end of the string).
