6965c9405a51c15572ed9ecf820bf0e53f66a0a5
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-10-27 08:30:02 -0300

    feat: add solution to 2023-10-27/J

--- 2023-10-27/J/J.cpp
+++ 2023-10-27/J/J.cpp
@@ -3,8 +3,30 @@
 using namespace std;
 
 using I = int64_t;
+using VS = vector<string>;
+
+bool has_prefix(const VS & a) {
+    for (I i = 0; i < a.size() - 1; ++i) {
+        if (a[i+1].find(a[i]) != string::npos) {
+            return true;
+        }
+    }
+    return false;
+}
 
 int main () {
-    cout << "Hello, World!" << endl;
+    I T;
+    cin >> T;
+    while (T--) {
+        I n;
+        cin >> n;
+        VS a(n);
+        for (auto & ai : a) {
+            cin >> ai;
+        }
+        sort(a.begin(), a.end());
+        auto is_consistent = has_prefix(a) ? "NO" : "YES";
+        cout << is_consistent << endl;
+    }
     return 0;
 }
--- 2023-10-27/J/README.md
+++ 2023-10-27/J/README.md
@@ -1,2 +1,8 @@
-# [PROBLEM_NAME](PROBLEM_LINK)
+# [UVa - 11362 - Phone List](https://onlinejudge.org/index.php?option=onlinejudge&Itemid=8&page=show_problem&problem=2347)
 
+1. get all strings in a list (vector in c++);
+2. sort the list;
+   1. notice that, if a number `x` is the prefix of another number `y`, then `x` comes right before `y` in the sorted list;
+3. starting in the beginning of the list, check if the current number is in the next number;
+   1. if it is, then the list is not consistent;
+   2. if, for all numbers, it is not, then the list is consistent.
