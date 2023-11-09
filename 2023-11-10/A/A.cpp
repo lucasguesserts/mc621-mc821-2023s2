75fb301a86a24adfdc4737a1e94f722dd345315d
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-11-09 14:15:31 -0300

    feat: add solution to 2023-11-10/A

--- 2023-11-10/A/A.cpp
+++ 2023-11-10/A/A.cpp
@@ -3,8 +3,31 @@
 using namespace std;
 
 using I = int64_t;
+using R = double;
 
-int main () {
-    cout << "Hello, World!" << endl;
+constexpr R EIGHT = 8.0;
+
+int main() {
+    I T;
+    cin >> T;
+    while (T--) {
+        R BX, BY;
+        I N;
+        cin >> BX >> BY;
+        cin >> N;
+        bool found = false;
+        for (I i = 0; i < N; ++i) {
+            R CX, CY;
+            cin >> CX >> CY;
+            if (hypot(BX - CX, BY - CY) <= EIGHT) {
+                found = true;
+            }
+        }
+        if (found) {
+            cout << "light a candle" << endl;
+        } else {
+            cout << "curse the darkness" << endl;
+        }
+    }
     return 0;
 }
--- 2023-11-10/A/README.md
+++ 2023-11-10/A/README.md
@@ -1,2 +1,7 @@
-# [PROBLEM_NAME](PROBLEM_LINK)
+# [Curse the Darkness](https://open.kattis.com/problems/cursethedarkness)
 
+Let $b_x, b_y \in \mathbb{R}_+$ be the coordinates of the book and $c_x, c_y \in \mathbb{R}_+$ be the coordinates of a candle. If there is a candle for which $\sqrt{(b_x - c_x)^2 + (b_y - c_y)^2} \leq 8$, then output `light a candle`. Otherwise, output `curse the darkness`.
+
+Complexity: $\mathcal{O}(n)$ where $n$ is the number of candles.
+
+Obs: read all the input even if you find a value for which the condition above holds.
