12d367ec8d19a1e9234285d893e939a4ceb49ae8
Samuel Plaça <samuplaza@gmail.com>
2023-11-09 20:03:56 -0300

    feat: add solution to 2023-11-10/J

--- 2023-11-10/J/J.cpp
+++ 2023-11-10/J/J.cpp
@@ -1,10 +1,38 @@
 #include <bits/stdc++.h>
 
+#define SIGN(x) ((x) > 0 ? 1 : ((x) < 0 ? -1 : 0))
+
 using namespace std;
 
-using I = int64_t;
+struct point_i {
+    int x, y;
+    point_i() { x = y = 0; }
+    point_i(int _x, int _y) : x(_x), y(_y) {}
+};
+
+point_i read_point() {
+    int x, y;
+    cin >> x >> y;
+    return point_i(x, y);
+}
 
 int main () {
-    cout << "Hello, World!" << endl;
+    int n;
+    while ((cin >> n) && n) {
+        vector<point_i> points;
+        for (int i = 0; i < n; i++)
+            points.push_back(read_point());
+        point_i center = points[n/2];
+
+        unsigned stan = 0, ollie = 0;
+        for (auto p : points) {
+            int relative_position = SIGN(p.x - center.x)*SIGN(p.y - center.y);
+            if (relative_position == 1)
+                stan++;
+            else if (relative_position == -1)
+                ollie++;
+        }
+        cout << stan << " " << ollie << endl;
+    }
     return 0;
 }
