0a2090bda32e3626bfd68dbac4b22a9e0045c0f7
Lucas Guesser Targino da Silva <lucasguesserts@gmail.com>
2023-11-09 15:29:33 -0300

    feat: add solution to 2023-11-10/I

--- 2023-11-10/I/I.cpp
+++ 2023-11-10/I/I.cpp
@@ -3,8 +3,31 @@
 using namespace std;
 
 using I = int64_t;
+using D = double;
+using C = complex<D>;
 
 int main () {
-    cout << "Hello, World!" << endl;
+    I i = 0;
+    while (true) {
+        D x, y;
+        I r;
+        cin >> x;
+        if (cin.eof()) { break; }
+        cin >> y >> r;
+        ++i;
+        // cout << x << " " << y << " " << r << endl;
+        C c(x, y);
+        C z(0, 0);
+        bool diverges = false;
+        for (I j = 0; j < r; j++) {
+            z = z*z + c;
+            if (abs(z) > 2.0) {
+                diverges = true;
+                break;
+            }
+        }
+        auto answer = diverges ? "OUT" : "IN";
+        cout << "Case " << i << ": " << answer << endl;
+    }
     return 0;
 }
--- 2023-11-10/I/README.md
+++ 2023-11-10/I/README.md
@@ -1,2 +1,3 @@
-# [PROBLEM_NAME](PROBLEM_LINK)
+# [Mandelbrot](https://open.kattis.com/problems/mandelbrot)
 
+Use `complex` from c++ and `abs` (which computes its absolute value). The solution is straightforward, just follow the procedured described in the problem statement.
