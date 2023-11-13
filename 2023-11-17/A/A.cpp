4bc44008be73d278866925671f0cd5fb2cc60d97
Samuel Plaça <samuplaza@gmail.com>
2023-11-13 13:40:17 -0300

    feat: add solution to 2023-11-17/A

--- 2023-11-17/A/A.cpp
+++ 2023-11-17/A/A.cpp
@@ -1,10 +1,13 @@
-#include <bits/stdc++.h>
+#include <iostream>
+
+#define PI  3.14159265358979323846
 
 using namespace std;
 
-using I = int64_t;
-
-int main () {
-    cout << "Hello, World!" << endl;
+int main() {
+    double r;
+    unsigned in, total;
+    while ((cin >> r >> total >> in) && total)
+        cout << (PI*r*r) << " " << (4*r*r)*((1.0*in)/total) << endl;
     return 0;
 }
