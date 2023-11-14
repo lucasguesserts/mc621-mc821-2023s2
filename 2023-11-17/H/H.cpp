3d0bde77c5628a0796fbb5a202654696e738ffec
Samuel Plaça <samuplaza@gmail.com>
2023-11-14 20:12:03 -0300

    feat: add solution to 2023-11-17/H

--- 2023-11-17/H/H.cpp
+++ 2023-11-17/H/H.cpp
@@ -1,10 +1,38 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-using I = int64_t;
+struct point_i {
+    int x, y;
+    point_i() { x = y = 0.0; }
+    point_i(int _x, int _y) : x(_x), y(_y) {}
+};
 
-int main () {
-    cout << "Hello, World!" << endl;
+// returns signed area of polygon P (negative means points in clockwise)
+double signed_area(const vector<point_i> & P) {
+    int n = P.size();
+    int64_t ans = 0;
+    for (int i = 0; i < n; ++i) {
+        int j = (i + 1) % n;
+        ans += (P[i].x*P[j].y - P[j].x*P[i].y);
+    }
+    return ans/2.0;
+}
+
+int main() {
+    cout << fixed;
+    cout << setprecision(1);
+    int npoints;
+    while ((cin >> npoints) && npoints) {
+        vector<point_i> vertices(npoints);
+        for (int i = 0; i < npoints; ++i) {
+            int x, y;
+            cin >> x >> y;
+            vertices[i] = point_i(x, y);
+        }
+        double area = signed_area(vertices);
+        if (area > 0)
+            cout << "C";
+        cout << "CW " << fabs(area) << endl;
+    }
     return 0;
 }
--- 2023-11-17/H/README.md
+++ 2023-11-17/H/README.md
@@ -1,2 +1,19 @@
-# [PROBLEM_NAME](PROBLEM_LINK)
+# [Kattis - Polygon Area](https://open.kattis.com/problems/polygonarea)
 
+This is a direct application of the [Shoelace Formula](https://en.wikipedia.org/wiki/Shoelace_formula) for computing
+the area of a polygon.
+
+Simply put, if a polygon~$P$ is described by the points $(x_0, y_0), \ldots, (x_{n-1}, y_{n-1})$, in either clockwise
+or counterclockwise order, then
+
+$$ \sum_{i=0}^{n-1} x_iy_{i+1} - x_{i+1}y_i $$
+
+is 2 times the *signed area* of $P$. Here we are considering $(x_n, y_n) = (x_0, y_n)$.
+
+In this problem, the coordinates are all integer and thus the above
+formula gives an integer result (therefore, the actual area is half-integer).
+
+The signed area of $A$ is its area with
+
+* negative sign, if the points are in clockwise order;
+* positive sign, otherwise.
